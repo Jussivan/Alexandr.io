0ea6ec48bb5a20da1e90054f73b907f7f3538bb2
Jussivan <jussivanbm1029@gmail.com>
2025-02-07 15:09:47 -0300

    add Update function to routes

--- Alexandr.io/routes/routes.js
+++ Alexandr.io/routes/routes.js
@@ -27,8 +27,8 @@ Router.get('/findAll', async (req,res) => {
     try {
         const book = await prisma.book.findMany();
         res.status(200).json({message: "Livros listados com sucesso!", book});
-    } catch (error) {
-        res.status(500).json({message: err.name})
+    } catch (err) {
+        res.status(500).json({message: err})
     }
 })
 
@@ -37,8 +37,28 @@ Router.get('/findBook', async (req,res) => {
         const { id } = req.body;
         const book = await prisma.book.findFirst({where: { id : id }})
         res.status(200).json({message: "Livro listado com sucesso!", book});
-    } catch (error) {
-        res.status(500).json({message: err.name})
+    } catch (err) {
+        res.status(500).json({message: err})
+    }
+})
+
+Router.put('/update', async (req, res) => {
+    try {
+        const book = req.body;
+        await prisma.book.update({
+            where: { id : book.id},
+            data: {
+                title: book.title,
+                author: book.author,
+                description: book.description,
+                genre: book.genre,
+                coverImage: book.coverImage,
+                pdfUrl: book.pdfUrl,
+            }
+        });
+        res.status(201).json({message: "Livro Atualizado com Sucesso!", book});
+    } catch (err) {
+        res.status(500).json({message: err})
     }
 })
 
@@ -47,8 +67,8 @@ Router.delete('/delete', async (req,res) => {
         const { id } = req.body;
         const book = await prisma.book.delete({where: { id : id }})
         res.status(200).json({message: "Livro deletado com sucesso!", book});
-    } catch (error) {
-        res.status(500).json({message: err.name})
+    } catch (err) {
+        res.status(500).json({message: err})
     }
 })
 
